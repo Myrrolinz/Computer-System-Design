0c7da0398ef2399522d55d1cbfe66a55108e2159
tracer-ics2017 <tracer@njuics.org>
2023-04-13 11:34:49 +0800

    >  compile 2013750 myrrolinz Linux Myrrolinz 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:34:46 up  1:11,  0 users,  load average: 0.22, 0.05, 0.02 f4bc2f4a306ca567d9448e2e389b573e872de4ad

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -81,17 +81,17 @@ make_EHelper(cwtl) {
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
-make_EHelper(movsx) {
+make_EHelper(movsx) { 
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }
 
-make_EHelper(movzx) {
+make_EHelper(movzx) { 
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   operand_write(id_dest, &id_src->val);
-  print_asm_template2(movzx);
+  print_asm_template2(movzx); 
 }
 
 make_EHelper(lea) {
